bb2eebdce211190a343ba4a066d41f2c99830259
SolunarNexus <adam.valent88@gmail.com>
2025-12-06 13:46:51 +0100

    Solve part one

--- day-03-lobby/src/main/kotlin/Main.kt
+++ day-03-lobby/src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val solution = Solution(test = false)
+    val totalJoltage = solution.totalJoltage()
+    println("Total joltage is $totalJoltage")
+
+    if (solution.isTest) {
+        val expectedTotalJoltage = 357L
+        assert(
+            totalJoltage == expectedTotalJoltage,
+            { "Incorrect total joltage. Was $totalJoltage, expected $expectedTotalJoltage" })
+        println("Solution verified for TEST")
+    } else {
+        val expectedTotalJoltage = 17113L
+        assert(
+            totalJoltage == expectedTotalJoltage,
+            { "Incorrect total joltage. Was $totalJoltage, expected $expectedTotalJoltage" })
+        println("Solution verified for PROD")
+    }
 }
--- day-03-lobby/src/main/kotlin/Solution.kt
+++ day-03-lobby/src/main/kotlin/Solution.kt
@@ -1,4 +1,43 @@
 package org.example
 
+import java.io.File
+
 class Solution {
+    val inputPath = "day-03-lobby/src/main/resources/input.txt"
+    val testInputPath = "day-03-lobby/src/main/resources/test_input.txt"
+    var isTest: Boolean
+
+    constructor(test: Boolean = false) {
+        if (test) println("### Initializing TEST solution ###") else println("### Initializing PROD solution ###")
+        this.isTest = test
+    }
+
+    fun totalJoltage(): Long {
+        val inputPath: String = if (isTest) testInputPath else inputPath
+        val banks = File(inputPath).inputStream().bufferedReader().readLines()
+        var totalJoltage = 0L
+
+        for (bank: String in banks) {
+            val it = bank.drop(1).iterator()
+            var firstBattery = bank.first()
+            var secondBattery = '0'
+
+            while (it.hasNext()) {
+                val current = it.next()
+
+                if (it.hasNext() && current > firstBattery) {
+                    firstBattery = current
+                    secondBattery = '0'
+                } else if (current > secondBattery) {
+                    secondBattery = current
+                }
+            }
+
+            val joltage: Int = String(charArrayOf(firstBattery, secondBattery)).toInt()
+            println("Joltage for $bank is $joltage")
+            totalJoltage += joltage
+        }
+
+        return totalJoltage
+    }
 }
