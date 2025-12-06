0cb5b96adf4ba140ddce84bcd56e2501d5f950bf
SolunarNexus <adam.valent88@gmail.com>
2025-12-06 12:06:28 +0100

    Refactor

--- day-02-gift-shop/src/main/kotlin/Solution.kt
+++ day-02-gift-shop/src/main/kotlin/Solution.kt
@@ -3,30 +3,22 @@ package org.example
 import java.io.File
 
 class Solution {
-    var inputPath = "day-02-gift-shop/src/main/resources/input.txt"
+    val inputPath = "day-02-gift-shop/src/main/resources/input.txt"
+    val testInputPath = "day-02-gift-shop/src/main/resources/test_input.txt"
+    var isTest: Boolean
 
-    fun idsOfTwoEqualSequences(): Sequence<Long> {
-        var ids = sequenceOf<Long>()
-        val ranges = File(inputPath).inputStream().bufferedReader().readLine().split(',')
-
-        for (range: String in ranges) {
-            val start = range.substringBefore('-').toLong()
-            val end = range.substringAfter('-').toLong()
-
-            for (id: Long in start..end) {
-                val idValue = id.toString()
-                val idLength = idValue.length
-
-                if (idLength % 2 == 0 && containsEqualSequences(idValue, idLength / 2)) {
-                    ids += id
-                }
-            }
-        }
-        return ids
+    constructor(test: Boolean = false) {
+        if (test) println("### Initializing TEST solution ###") else println("### Initializing PROD solution ###")
+        this.isTest = test
     }
 
-    fun idsOfMultipleEqualSequences(): Sequence<Long> {
-        var ids = sequenceOf<Long>()
+    enum class Policy {
+        TWO_PARTS, N_PARTS
+    }
+
+    fun invalidIds(policy: Policy): Sequence<Long> {
+        val inputPath: String = if (isTest) testInputPath else inputPath
+        val ids = arrayListOf<Long>()
         val ranges = File(inputPath).inputStream().bufferedReader().readLine().split(',')
 
         for (range: String in ranges) {
@@ -34,18 +26,35 @@ class Solution {
             val end = range.substringAfter('-').toLong()
 
             for (id: Long in start..end) {
-                val idSeq = id.toString()
-                val mid = id.toString().length / 2
-
-                for (offset in (1..mid).filter { idSeq.length % it == 0 }) {
-                    if (containsEqualSequences(idSeq, offset)) {
-                        ids += id
-                        break
-                    }
+                when (policy) {
+                    Policy.TWO_PARTS -> processTwoPartId(id, ids)
+                    Policy.N_PARTS -> processNPartId(id, ids)
                 }
             }
         }
-        return ids
+
+        return ids.asSequence()
+    }
+
+    fun processTwoPartId(id: Long, ids: MutableList<Long>) {
+        val idSeq = id.toString()
+        if (idSeq.length % 2 == 0 && containsEqualSequences(idSeq, idSeq.length / 2)) {
+            ids.add(id)
+        }
+    }
+
+    fun processNPartId(id: Long, ids: MutableList<Long>) {
+        val idSeq = id.toString()
+        val idLength = idSeq.length
+        val mid = idLength / 2
+
+        for (offset in (1..mid).filter { idLength % it == 0 }) {
+            if (containsEqualSequences(idSeq, offset)) {
+                ids.add(id)
+                break
+            }
+        }
+
     }
 
     fun containsEqualSequences(id: String, offset: Int): Boolean {
@@ -53,8 +62,8 @@ class Solution {
         var seqStart = 0
 
         while (seqStart + 2 * offset - 1 < id.length) {
-            val currentSeq = id.slice(seqStart..< seqStart + offset)
-            val nextSeq = id.slice(seqStart + offset..< seqStart + 2 * offset)
+            val currentSeq = id.slice(seqStart..<seqStart + offset)
+            val nextSeq = id.slice(seqStart + offset..<seqStart + 2 * offset)
 
             if (currentSeq != nextSeq) {
                 return false
@@ -65,4 +74,8 @@ class Solution {
 
         return true
     }
+
+    fun sumOfTwoPartIds(): Long = invalidIds(Policy.TWO_PARTS).sum()
+
+    fun sumOfMultiplePartIds(): Long = invalidIds(Policy.N_PARTS).sum()
 }
