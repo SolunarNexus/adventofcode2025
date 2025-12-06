e64a3798456ddebaadb1f31631438c0dec59d60d
SolunarNexus <adam.valent88@gmail.com>
2025-12-06 16:11:37 +0100

    Prepare classes

--- day-04-printing-department/src/main/kotlin/Main.kt
+++ day-04-printing-department/src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package org.example
 
 fun main() {
-    println("Hello, world!")
+    val test = true
+    val inputPath = if (test) "day-03-lobby/src/main/resources/test_input.txt" else "day-03-lobby/src/main/resources/input.txt"
+
+    val solution = Solution(inputPath)
+//    val part1 = ...
+//    val part2 = ...
+//    println("Part 1 $part1")
+//    println("Part 2 $part2")
+
+    if (test) {
+//        assertResults(...)
+//        assertResults(...)
+//        println("Solution verified for TEST")
+    } else {
+//        assertResults(...)
+//        assertResults(...)
+//        println("Solution verified for PROD")
+    }
+}
+
+fun assertResults(expected: Long, actual: Long, part: String){
+    assert(expected == actual, { "Incorrect total joltage for part $part. Was $actual, expected $expected" })
 }
