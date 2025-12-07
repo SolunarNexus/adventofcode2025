32cd1ed6eaba9f6684359d385bfd1714f759054d
SolunarNexus <adam.valent88@gmail.com>
2025-12-07 16:59:02 +0100

    Solve part one

--- day-06-trash-compactor/src/main/kotlin/Main.kt
+++ day-06-trash-compactor/src/main/kotlin/Main.kt
@@ -2,25 +2,25 @@ package org.example
 
 fun main() {
     val test = false
-    val inputPath = if (test) "day-05-cafeteria/src/main/resources/test_input.txt" else "day-05-cafeteria/src/main/resources/input.txt"
+    val inputPath = if (test) "day-06-trash-compactor/src/main/resources/test_input.txt" else "day-06-trash-compactor/src/main/resources/input.txt"
 
     val solution = Solution(inputPath)
-//    val part1 = solution.availableFreshIngredientsCount()
+    val part1 = solution.grandTotal()
 //    val part2 = solution.allFreshIngredientsIdsCount()
-//    println("Part 1: available fresh ?ingredients count is $part1")
+    println("Part 1: the grand total of all the answers is $part1")
 //    println("Part 2: all fresh ingredients IDs count is $part2")
 
     if (test) {
-//        assertResults(3, part1, "Incorrect available fresh ingredients count for part one")
+        assertResults(4277556L, part1, "Incorrect grand total for part one")
 //        assertResults(14, part2, "Incorrect all possible fresh ingredients IDs count for part two")
-//        println("Solution verified for TEST")
+        println("Solution verified for TEST")
     } else {
-//        assertResults(661, part1, "Incorrect available fresh ingredients count for part one")
+        assertResults(6171290547579L, part1, "Incorrect grand total for part one")
 //        assertResults(359526404143208L, part2, "Incorrect all possible fresh ingredients IDs count for part two")
-//        println("Solution verified for PROD")
+        println("Solution verified for PROD")
     }
 }
 
-fun <T> assertResults(expected: T, actual: T, message: String){
+fun <T> assertResults(expected: T, actual: T, message: String) {
     assert(expected == actual) { "$message. Was $actual, expected $expected" }
 }
--- day-06-trash-compactor/src/main/kotlin/Solution.kt
+++ day-06-trash-compactor/src/main/kotlin/Solution.kt
@@ -1,9 +1,31 @@
 package org.example
 
+import java.io.File
+
 class Solution {
     val inputPath: String
 
     constructor(inputPath: String) {
         this.inputPath = inputPath
+    }
+
+    fun grandTotal(): Long = answers().sum()
+
+    private fun answers(): List<Long> {
+        val lines = File(inputPath).inputStream().bufferedReader().readLines()
+            .map { it.trim().split("\\s+".toRegex()) }
+        val worksheet = lines.dropLast(1)
+        val operations = lines.last()
+        val answers = mutableListOf<Long>()
+
+        for (i in 0..<worksheet.first().size) {
+            if (operations[i] == "+") {
+                answers.add(worksheet.sumOf { it[i].toLong() })
+            } else if (operations[i] == "*") {
+                answers.add(worksheet.fold(1L) { acc, row -> acc * row[i].toLong() })
+            }
+        }
+        println("Answers to problems are: ${answers.joinToString(", ")}")
+        return answers
     }
 }
