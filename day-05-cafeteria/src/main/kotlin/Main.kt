7d308e0242140f91f0820f4ea23077fee492365e
SolunarNexus <adam.valent88@gmail.com>
2025-12-07 12:57:14 +0100

    Solve part one

--- day-05-cafeteria/src/main/kotlin/Main.kt
+++ day-05-cafeteria/src/main/kotlin/Main.kt
@@ -1,26 +1,26 @@
 package org.example
 
 fun main() {
-    val test = true
-    val inputPath = if (test) "day-04-cafeteria/src/main/resources/test_input.txt" else "day-03-lobby/src/main/resources/input.txt"
+    val test = false
+    val inputPath = if (test) "day-05-cafeteria/src/main/resources/test_input.txt" else "day-05-cafeteria/src/main/resources/input.txt"
 
     val solution = Solution(inputPath)
-//    val part1 = solution.totalJoltage(2)
+    val part1 = solution.freshIngredientsCount()
 //    val part2 = solution.totalJoltage(12)
-//    println("Part 1 total joltage is $part1")
+    println("Part 1: fresh ingredients count is $part1")
 //    println("Part 2 total joltage is $part2")
 
     if (test) {
-//        assertResults(357L, part1, "one")
+        assertResults(3, part1, "one")
 //        assertResults(3121910778619L, part2, "two")
-//        println("Solution verified for TEST")
+        println("Solution verified for TEST")
     } else {
-//        assertResults(17113L, part1, "one")
+        assertResults(661, part1, "one")
 //        assertResults(169709990062889L, part2, "two")
-//        println("Solution verified for PROD")
+        println("Solution verified for PROD")
     }
 }
 
-fun assertResults(expected: Long, actual: Long, message: String){
+fun <T> assertResults(expected: T, actual: T, message: String){
     assert(expected == actual) { "$message. Was $actual, expected $expected" }
 }
--- day-05-cafeteria/src/main/kotlin/Solution.kt
+++ day-05-cafeteria/src/main/kotlin/Solution.kt
@@ -1,5 +1,7 @@
 package org.example
 
+import java.io.File
+
 class Solution {
     val inputPath: String
 
@@ -7,4 +9,23 @@ class Solution {
         this.inputPath = inputPath
     }
 
+    fun freshIngredientsCount(): Int = freshIngredientIds().size
+
+    private fun freshIngredientIds(): List<Long> {
+        val database = File(inputPath).inputStream().bufferedReader().readText().split("\n\n")
+        val ranges = database[0].lines().map {
+            Pair(it.substringBefore('-'), it.substringAfter('-'))
+        }
+        val available = database[1].lines().map { it.toLong() }
+        val freshIds = mutableListOf<Long>()
+
+        for (id in available) {
+            if (ranges.any { (start, end) -> id in start.toLong()..end.toLong() }) {
+                freshIds.add(id)
+            }
+        }
+
+        println("Fresh ingredients IDs are: ${freshIds.joinToString(", ")}")
+        return freshIds
+    }
 }
