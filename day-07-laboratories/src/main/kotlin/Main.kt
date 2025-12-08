1cccafbcf103b895aa0d8ed01702fa139e78cb3c
Oskar Adam Valent <oskar.valent@siko.cz>
2025-12-08 14:50:57 +0100

    Solve part one

--- day-07-laboratories/src/main/kotlin/Main.kt
+++ day-07-laboratories/src/main/kotlin/Main.kt
@@ -4,18 +4,18 @@ fun main() {
     val test = false
     val inputPath = if (test) "day-07-laboratories/src/main/resources/test_input.txt" else "day-07-laboratories/src/main/resources/input.txt"
 
-//    val solution = Solution(inputPath)
-//    val part1 = solution.answersWithSimpleMath().sum()
+    val solution = Solution(inputPath)
+    val part1 = solution.beamSplitSum()
 //    val part2 = solution.answersWithCephalopodMath().sum()
-//    println("Part 1: the grand total of all the answers is $part1")
+    println("Part 1: tachyon beam splits ${part1}x")
 //    println("Part 2: the grand total of all the answers applying cephalopod math is $part2")
 
     if (test) {
-//        assertResults(4277556L, part1, "Incorrect grand total for part one")
+        assertResults(21, part1, "Incorrect beam split count for part one")
 //        assertResults(6171290547579L, part2, "Incorrect grand total for part two")
-//        println("Solution verified for TEST")
+        println("Solution verified for TEST")
     } else {
-//        assertResults(6171290547579L, part1, "Incorrect grand total for part one")
+        assertResults(1524, part1, "Incorrect beam split count for part one")
 //        assertResults(8811937976367L, part2, "Incorrect grand total for part two")
 //        println("Solution verified for PROD")
     }
--- day-07-laboratories/src/main/kotlin/Solution.kt
+++ day-07-laboratories/src/main/kotlin/Solution.kt
@@ -3,11 +3,31 @@ package org.example
 import java.io.File
 
 class Solution {
-    val inputPath: String
     val input: List<String>
 
     constructor(inputPath: String) {
-        this.inputPath = inputPath
         this.input = File(inputPath).inputStream().bufferedReader().readLines()
+    }
+
+    fun beamSplitSum(): Int {
+        val (startX, startY) = Pair(input[0].indexOf('S'), 1)
+        val manifold = input.toMutableList()
+        val result = propagateBeam(manifold, startX, startY)
+        manifold.forEach { println(it) }
+        return result
+    }
+
+    private fun propagateBeam(manifold: MutableList<String>, x: Int, y: Int): Int {
+        if (y >= manifold.size) return 0
+
+        when (manifold[y][x]) {
+            '|' -> return 0
+            '.' -> {
+                manifold[y] = manifold[y].withIndex().map { (idx, it) -> if (idx == x) '|' else it }.joinToString("")
+                return propagateBeam(manifold, x, y + 1)
+            }
+            '^' -> return  1 + propagateBeam(manifold, x - 1, y) + propagateBeam(manifold, x + 1, y)
+            else -> throw IllegalArgumentException("Invalid character: ${manifold[y][x]}")
+        }
     }
 }
