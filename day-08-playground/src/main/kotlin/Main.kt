7e1a0074bbf572998300c942d78da6be938cfd17
SolunarNexus <adam.valent88@gmail.com>
2025-12-09 14:35:37 +0100

    Solve part one

--- day-08-playground/src/main/kotlin/Main.kt
+++ day-08-playground/src/main/kotlin/Main.kt
@@ -4,21 +4,27 @@ fun main() {
     val test = false
     val inputPath =
         if (test) "day-08-playground/src/main/resources/test_input.txt" else "day-08-playground/src/main/resources/input.txt"
+    val connectionsAvailable = if (test) 10 else 1000
 
-//    val solution = Solution(inputPath)
-//    val part1 = solution.beamSplitSum()
+    val solution = Solution(inputPath)
+    val circuits = solution.circuits(connectionsAvailable)
+    val circuitSizes = circuits.map { it.size }.sortedDescending()
+    val part1 = circuitSizes.take(3).fold(1) { acc, i -> acc * i }
+    circuits.sortedBy { it.size }.forEach { println(it.joinToString(separator = " ")) }
+    println("Circuit sizes sorted: $circuitSizes (n=${circuitSizes.size})")
+
 //    val part2 = solution.quantumBeamSplitSum()
-//    println("Part 1: tachyon beam splits ${part1}x")
+    println("Part 1: product of the first three biggest circuits = $part1")
 //    println("Part 2: quantum tachyon beam splits ${part2}x")
 
     if (test) {
-//        assertResults(21, part1, "Incorrect beam split count for part one")
+        assertResults(40, part1, "Incorrect sum for the first three biggest circuits for part one")
 //        assertResults(40L, part2, "Incorrect grand total for part two")
-//        println("Solution verified for TEST")
+        println("Solution verified for TEST")
     } else {
-//        assertResults(1524, part1, "Incorrect beam split count for part one")
+        assertResults(105952, part1, "Incorrect sum for the first three biggest circuits for part one")
 //        assertResults(32982105837605L, part2, "Incorrect grand total for part two")
-//        println("Solution verified for PROD")
+        println("Solution verified for PROD")
     }
 }
 
--- day-08-playground/src/main/kotlin/Solution.kt
+++ day-08-playground/src/main/kotlin/Solution.kt
@@ -1,11 +1,70 @@
 package org.example
 
 import java.io.File
+import kotlin.collections.List
+import kotlin.collections.plus
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+typealias Coordinate = Triple<Int, Int, Int>
+typealias Connection = Pair<Coordinate, Coordinate>
+typealias Circuit = List<Coordinate>
 
 class Solution {
     val input: List<String>
 
     constructor(inputPath: String) {
         this.input = File(inputPath).inputStream().bufferedReader().readLines()
+    }
+
+    fun circuits(connectionsAvailable: Int = 10) = computeCircuits(coordinates(), connectionsAvailable)
+
+    private fun allConnections(coordinates: List<Coordinate>): List<Connection> =
+        coordinates.flatMapIndexed { i, coord ->
+            coordinates.drop(i + 1).map { other -> Pair(coord, other) }
+        }
+
+    private fun coordinates(): List<Coordinate> = input.map { line ->
+        val (x, y, z) = line.split(',').map { it.toInt() }
+        Triple(x, y, z)
+    }
+
+    private fun distance(connection: Connection): Double {
+        val (a, b, c) = connection.first
+        val (x, y, z) = connection.second
+        return sqrt((a - x.toDouble()).pow(2) + (b - y.toDouble()).pow(2) + (c - z.toDouble()).pow(2))
+    }
+
+    private fun computeDistances(connections: List<Connection>): List<Pair<Connection, Double>> =
+        connections.map { Pair(it, distance(it)) }
+
+    private fun insertIntoCircuit(connection: Connection, circuits: MutableList<Circuit>) {
+        val (posA, posB) = connection
+
+        if (circuits.any { it.contains(posA) && it.contains(posB) }) {
+            return
+        } else if (circuits.any { it.contains(posA) } && circuits.none { it.contains(posB) }) {
+            val target = circuits.indexOfFirst { it.contains(posA) }
+            circuits[target] += posB
+        } else if (circuits.any { it.contains(posB) } && circuits.none { it.contains(posA) }) {
+            val target = circuits.indexOfFirst { it.contains(posB) }
+            circuits[target] += posA
+        } else {
+            val circuitA = circuits.indexOfFirst { it.contains(posA) }
+            val circuitB = circuits.indexOfFirst { it.contains(posB) }
+            circuits[circuitA] = circuits[circuitA] + circuits[circuitB]
+            circuits.removeAt(circuitB)
+        }
+    }
+
+    private fun computeCircuits(coordinates: List<Coordinate>, connectionsAvailable: Int): List<Circuit> {
+        val distances = computeDistances(allConnections(coordinates)).sortedBy { it.second }
+        val circuits = coordinates.map { listOf(it) }.toMutableList()
+
+        for (connection in distances.map { it.first }.take(connectionsAvailable)) {
+            insertIntoCircuit(connection, circuits)
+        }
+
+        return circuits
     }
 }
