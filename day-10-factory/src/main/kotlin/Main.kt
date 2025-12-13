ea0e5c9dec6f6b8fe9739ceac1fdd9f7e5733ffb
SolunarNexus <adam.valent88@gmail.com>
2025-12-13 18:41:08 +0100

    Solve part two

--- day-10-factory/src/main/kotlin/Main.kt
+++ day-10-factory/src/main/kotlin/Main.kt
@@ -6,18 +6,18 @@ fun main() {
         if (test) "day-10-factory/src/main/resources/test_input.txt" else "day-10-factory/src/main/resources/input.txt"
 
     val solution = Solution(inputPath)
-    val part1 = solution.sumOfButtonPresses()
-//    val part2 = solution.largestRectangleAreWithinBounds()
-    println("Part 1: the minimum number of button presses is $part1")
-//    println("Part 2: the largest rectangle area within bounds is $part2")
+    val part1 = solution.sumOfButtonPressesForLights()
+    val part2 = solution.sumOfButtonPressesForJoltage()
+    println("Part 1: the minimum number of button presses for correct light configuration is $part1")
+    println("Part 2: the minimum number of button presses for correct joltage configuration is $part2")
 
     if (test) {
         assertResults(7, part1, "Incorrect sum of button presses for part one")
-//        assertResults(24L, part2, "Incorrect rectangle area for part two")
+        assertResults(33, part2, "Incorrect sum of button presses for part two")
         println("Solution verified for TEST")
     } else {
         assertResults(558, part1, "Incorrect sum of button presses for part one")
-//        assertResults(1513792010L, part2, "Incorrect rectangle area for part two")
+        assertResults(20317, part2, "Incorrect sum of button presses for part two")
         println("Solution verified for PROD")
     }
 }
--- day-10-factory/src/main/kotlin/Solution.kt
+++ day-10-factory/src/main/kotlin/Solution.kt
@@ -1,66 +1,16 @@
 package org.example
 
 import java.io.File
-import kotlin.math.pow
-
-private data class Schematic(val lights: Int, val buttons: List<Int>, val joltage: List<Int>) {
-
-    companion object {
-        fun of(input: String): Schematic =
-            Schematic(
-                lights = input.lightsToInt(),
-                buttons = input
-                    .substringAfter(" ")
-                    .substringBefore(" {")
-                    .split(" ")
-                    .map { it.buttonsToInt() },
-                joltage = input.substringAfter("{")
-                    .substringBefore("}")
-                    .split(",")
-                    .map { it.toInt() }
-            )
-
-        private fun String.lightsToInt(): Int =
-            this.substringAfter("[").substringBefore("]").reversed()
-                .fold(0) { carry, next ->
-                    (carry shl 1) or if (next == '#') 1 else 0
-                }
-
-        private fun String.buttonsToInt(): Int =
-            this.substringAfter("(").substringBefore(")")
-                .split(",")
-                .fold(0) { carry, next ->
-                    carry or (2 pow next.toInt())
-
-                }
-
-        infix fun Int.pow(exp: Int): Int =
-            this.toDouble().pow(exp.toDouble()).toInt()
-
-    }
-}
 
 class Solution {
-    private val schematics: List<Schematic>
+    private val factory: Factory
 
     constructor(inputPath: String) {
-        schematics = File(inputPath).inputStream().bufferedReader().readLines()
-            .map { Schematic.of(it) }
+        val lines = File(inputPath).inputStream().bufferedReader().readLines()
+        this.factory = Factory.parse(lines)
     }
 
-    fun sumOfButtonPresses(): Int = schematics.sumOf { minimalButtonPresses(it) }
+    fun sumOfButtonPressesForLights(): Long = factory.minTotalButtonPresses()
 
-    private fun minimalButtonPresses(schematic: Schematic): Int =
-        schematic.buttons
-            .combinations()
-            .sortedBy { it.size }
-            .first { set ->
-                set.fold(0) { c, n -> c xor n } == schematic.lights
-            }.size
-
-    private fun <T> List<T>.combinations(): List<Set<T>> =
-        if (isEmpty()) listOf(emptySet())
-        else drop(1).combinations().let { powerSetRest ->
-            powerSetRest + powerSetRest.map { it + first() }
-        }
+    fun sumOfButtonPressesForJoltage(): Long = factory.minTotalJoltagePresses()
 }
