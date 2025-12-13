89f9a447788e47bcaee14058336aa60b5bf4d6cc
SolunarNexus <adam.valent88@gmail.com>
2025-12-13 16:01:44 +0100

    Solve part one

--- day-10-factory/src/main/kotlin/Main.kt
+++ day-10-factory/src/main/kotlin/Main.kt
@@ -6,19 +6,19 @@ fun main() {
         if (test) "day-10-factory/src/main/resources/test_input.txt" else "day-10-factory/src/main/resources/input.txt"
 
     val solution = Solution(inputPath)
-//    val part1 = solution.largestRectangleArea()
+    val part1 = solution.sumOfButtonPresses()
 //    val part2 = solution.largestRectangleAreWithinBounds()
-//    println("Part 1: the largest rectangle area is $part1")
+    println("Part 1: the minimum number of button presses is $part1")
 //    println("Part 2: the largest rectangle area within bounds is $part2")
 
     if (test) {
-//        assertResults(50L, part1, "Incorrect rectangle area for part one")
+        assertResults(7, part1, "Incorrect sum of button presses for part one")
 //        assertResults(24L, part2, "Incorrect rectangle area for part two")
-//        println("Solution verified for TEST")
+        println("Solution verified for TEST")
     } else {
-//        assertResults(4738108384L, part1, "Incorrect rectangle area for part one")
+        assertResults(558, part1, "Incorrect sum of button presses for part one")
 //        assertResults(1513792010L, part2, "Incorrect rectangle area for part two")
-//        println("Solution verified for PROD")
+        println("Solution verified for PROD")
     }
 }
 
