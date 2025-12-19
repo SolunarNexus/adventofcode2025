4964768ebf5c28b3888f81dc593964211989fdc3
SolunarNexus <adam.valent88@gmail.com>
2025-12-19 10:00:43 +0100

    Solve part one\

--- day-11-reactor/src/main/kotlin/Main.kt
+++ day-11-reactor/src/main/kotlin/Main.kt
@@ -5,18 +5,18 @@ fun main() {
     val inputPath =
         if (test) "day-11-reactor/src/main/resources/test_input.txt" else "day-11-reactor/src/main/resources/input.txt"
 
-//    val solution = Solution(inputPath)
-//    val part1 = solution.sumOfButtonPressesForLights()
+    val solution = Solution(inputPath)
+    val part1 = solution.paths("you")
 //    val part2 = solution.sumOfButtonPressesForJoltage()
-//    println("Part 1: the minimum number of button presses for correct light configuration is $part1")
+    println("Part 1: number of paths leading from you to out is $part1")
 //    println("Part 2: the minimum number of button presses for correct joltage configuration is $part2")
 
     if (test) {
-//        assertResults(7, part1, "Incorrect sum of button presses for part one")
+        assertResults(5, part1, "Incorrect number of paths for part one")
 //        assertResults(33, part2, "Incorrect sum of button presses for part two")
         println("Solution verified for TEST")
     } else {
-//        assertResults(558, part1, "Incorrect sum of button presses for part one")
+        assertResults(683, part1, "Incorrect number of paths for part one")
 //        assertResults(20317, part2, "Incorrect sum of button presses for part two")
         println("Solution verified for PROD")
     }
--- day-11-reactor/src/main/kotlin/Solution.kt
+++ day-11-reactor/src/main/kotlin/Solution.kt
@@ -1,11 +1,28 @@
 package org.example
 
 import java.io.File
+import java.util.LinkedList
+import java.util.Queue
 
 class Solution {
+    private val graph: Map<String, List<String>>
 
     constructor(inputPath: String) {
         val lines = File(inputPath).inputStream().bufferedReader().readLines()
+        this.graph = lines.map { it.split(":") }.associate { it[0] to it[1].split(" ").map { part -> part.trim()} }
     }
 
+    fun paths(start: String): Int {
+        val queue: Queue<String> = LinkedList(listOf(start))
+        var paths = 0
+
+        while(queue.isNotEmpty()){
+            val current = queue.poll()
+            if(current == "out"){
+                paths++
+            }
+            graph[current]?.forEach { queue.add(it) }
+        }
+        return paths
+    }
 }
